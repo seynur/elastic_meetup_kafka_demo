8f072f1dd0be669f89a90761b2b254c40e2288c7
selim <selim@seynur.com>
2018-02-11 19:31:12 +0300

    Updated scripts

--- kafka-elastic/all.sh
+++ kafka-elastic/all.sh
@@ -3,4 +3,5 @@
 # Run all playbooks at once
 ansible-playbook /kafka-elastic/1_configure-elasticsearch.yml
 ansible-playbook /kafka-elastic/2_configure-kibana.yml
-ansible-playbook /kafka-elastic/3_configure-mysql.yml  
+ansible-playbook /kafka-elastic/3_configure-logstash.yml
+ansible-playbook /kafka-elastic/4_configure-mysql.yml
